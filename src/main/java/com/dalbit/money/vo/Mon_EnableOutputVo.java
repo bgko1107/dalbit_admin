d5a650a78c6a1154d3702d9b4bd29d2ae8a34295
고병권 <bgko@inforex.co.kr>
2021-01-12 11:15:57 +0900

    환전 내역 리스트 변경 및 수정

--- src/main/java/com/dalbit/money/vo/Mon_EnableOutputVo.java
+++ src/main/java/com/dalbit/money/vo/Mon_EnableOutputVo.java
@@ -23,6 +23,8 @@ public class Mon_EnableOutputVo {
     private int specialCnt;
     private int type;
     private String memtype;
+    private int gold_old;
+    private int mod_gold;            // 나머지 별
 
     private int byeol;
     private int cash_real;
